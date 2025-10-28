d890f950cb6b8d4bdbcb1df5d4ddcde529a32c22
juhanatarvainen-a11y <juhana.tarvainen@gmail.com>
2025-10-28 14:11:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Tervetuloa Erakkomajaan!"
+    return "Tervetuloa Evankeliseen erakkomajaan, jossa voit hengähtää ja hiljentyä Raamatun sanan ja rukouksen äärellä."
